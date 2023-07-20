0f5ca85152dfc49bb75f66ccf8a7962f01d46cb1
Stanislav <stasikparmut@yandex.ru>
2023-07-21 01:04:58 +0300

    matchers fix

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -2,34 +2,27 @@ import { Post, PostsService } from './posts.service';
 
 describe('PostsService', () => {
   let postsService: PostsService;
+  const post: Omit<Post, 'id' | 'date'> = {
+    text: 'Mocked post',
+  };
 
-  beforeEach(() => {
+  beforeEach(async () => {
     postsService = new PostsService();
+
+    postsService.create({ text: 'Some pre-existing post' });
+  });
+  
+  it("should add a new post", () => {
+
+    const createdPost = postsService.create(post)
+    expect(createdPost).toEqual(expect.any(Post))
+
   });
 
-  it('должен добавить новый пост', () => {
-    const post: Omit<Post, 'id' | 'date'> = {
-      text: 'Mocked post',
-    };
-
-    postsService.create(post);
-
-    // Проверяем, что пост был успешно добавлен
-    const addedPost = postsService.find(post.text);
-    expect(addedPost).toBeDefined();
-    expect(addedPost.text).toBe(post.text);
+  it("should find a post", () => {
+    const foundPost = postsService.find('1')
+    expect(foundPost)toEqual(expect.any(Post))
+    expect(foundPost).toMatchObject({...post, id:'1', date: expect.any(String)})
   });
 
-  it('должен найти пост', () => {
-    const preExistingPost: Omit<Post, 'id' | 'date'> = {
-      text: 'Some pre-existing post',
-    };
-    postsService.create(preExistingPost);
-
-    const foundPost = postsService.find(preExistingPost.text);
-
-    // Проверяем, что пост был успешно найден
-    expect(foundPost).toBeDefined();
-    expect(foundPost.text).toBe(preExistingPost.text);
-  });
 });
